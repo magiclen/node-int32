bac51f33803eb9c70f5619c5f8b59f2141a38ce4
Magic Len <len@magiclen.org>
2023-04-18 10:55:26 +0800

    use RangeError

--- src/lib.rs
+++ src/lib.rs
@@ -26,62 +26,87 @@ pub fn divide(a: i32, b: i32) -> i32 {
 }
 
 /// Computes `a ^ b`, wrapping around at the boundary of an 32-bit integer.
+///
+/// `b` must not be smaller than zero
 #[napi]
-pub fn pow(a: i32, b: i32) -> Result<i32> {
+pub fn pow(env: Env, a: i32, b: i32) -> Result<i32> {
     if b < 0 {
-        return Err(Error::from_reason(
+        env.throw_range_error(
             "the exponent of an integer number must not be smaller than zero",
-        ));
+            None,
+        )?;
+
+        return Err(Error::from_reason(""));
     }
 
     Ok(a.wrapping_pow(b as u32))
 }
 
 /// Computes `a << b`, wrapping around at the boundary of an 32-bit integer.
+///
+/// `b` must not be smaller than zero
 #[napi(js_name = "shiftLeft")]
-pub fn shift_left(a: i32, b: i32) -> Result<i32> {
+pub fn shift_left(env: Env, a: i32, b: i32) -> Result<i32> {
     if b < 0 {
-        return Err(Error::from_reason("the bit count for shift must not be smaller than zero"));
+        env.throw_range_error("the bit count for shift must not be smaller than zero", None)?;
+
+        return Err(Error::from_reason(""));
     }
 
     Ok(a.wrapping_shl(b as u32))
 }
 
 /// Computes `a >> b`, wrapping around at the boundary of an 32-bit integer.
+///
+/// `b` must not be smaller than zero
 #[napi(js_name = "shiftRight")]
-pub fn shift_right(a: i32, b: i32) -> Result<i32> {
+pub fn shift_right(env: Env, a: i32, b: i32) -> Result<i32> {
     if b < 0 {
-        return Err(Error::from_reason("the bit count for shift must not be smaller than zero"));
+        env.throw_range_error("the bit count for shift must not be smaller than zero", None)?;
+
+        return Err(Error::from_reason(""));
     }
 
     Ok(a.wrapping_shr(b as u32))
 }
 
 /// Computes `a >>> b`, wrapping around at the boundary of an 32-bit integer.
+///
+/// `b` must not be smaller than zero
 #[napi(js_name = "shiftRightUnsigned")]
-pub fn shift_right_unsigned(a: i32, b: i32) -> Result<i32> {
+pub fn shift_right_unsigned(env: Env, a: i32, b: i32) -> Result<i32> {
     if b < 0 {
-        return Err(Error::from_reason("the bit count for shift must not be smaller than zero"));
+        env.throw_range_error("the bit count for shift must not be smaller than zero", None)?;
+
+        return Err(Error::from_reason(""));
     }
 
     Ok((a as u32).wrapping_shr(b as u32) as i32)
 }
 
 /// Shifts the bits to the right by a specified amount n, wrapping the truncated bits to the beginning of the resulting 32-bit integer.
+///
+/// `b` must not be smaller than zero
 #[napi(js_name = "rotateLeft")]
-pub fn rotate_left(a: i32, b: i32) -> Result<i32> {
+pub fn rotate_left(env: Env, a: i32, b: i32) -> Result<i32> {
     if b < 0 {
-        return Err(Error::from_reason("the bit count for rotation must not be smaller than zero"));
+        env.throw_range_error("the bit count for rotation must not be smaller than zero", None)?;
+
+        return Err(Error::from_reason(""));
     }
 
     Ok(a.rotate_left(b as u32))
 }
 
 /// Shifts the bits to the right by a specified amount n, wrapping the truncated bits to the beginning of the resulting 32-bit integer.
+///
+/// `b` must not be smaller than zero
 #[napi(js_name = "rotateRight")]
-pub fn rotate_right(a: i32, b: i32) -> Result<i32> {
+pub fn rotate_right(env: Env, a: i32, b: i32) -> Result<i32> {
     if b < 0 {
-        return Err(Error::from_reason("the bit count for rotation must not be smaller than zero"));
+        env.throw_range_error("the bit count for rotation must not be smaller than zero", None)?;
+
+        return Err(Error::from_reason(""));
     }
 
     Ok(a.rotate_right(b as u32))
